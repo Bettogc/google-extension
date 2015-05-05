1635f66069c1ab01006e18bedacf7976b6a168d5
Ludwin Garcia <lgarcia@cognits.co>
2015-05-05 12:14:21 -0600

    Add documentation to background.js and change buttons names

--- js/background.js
+++ js/background.js
@@ -5,6 +5,7 @@ function clickHandler(e) {
 };
 
 function Reload(){
+    /* Este procedmiento llama y ejecuta el codigo de script.js */
     chrome.tabs.getSelected(null, function(tab) {
         chrome.tabs.executeScript(tab.id, {file: "js/script.js"});
     });
@@ -17,8 +18,8 @@ document.addEventListener('DOMContentLoaded', function() {
     var CheckStartButton = document.getElementById('start');
     var CheckStopButton = document.getElementById('stop');
     var TimeRefresh = document.getElementById('time-refresh');
-    /* creamos el evento si se clickea el boton start*/
 
+    /* creamos el evento si se clickea el boton start*/
     CheckStartButton.addEventListener('click', function() {
         if (TimeRefresh.value < 1) {
             alert("Ingreso Erroneo");
@@ -27,6 +28,7 @@ document.addEventListener('DOMContentLoaded', function() {
         };
     }, false);
 
+    /* creamos el evento si se clickea el boton false*/
     CheckStopButton.addEventListener('click', function() {
         clickHandler();
     }, false);
