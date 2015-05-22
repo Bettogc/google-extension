a594aaf2f7bcc0538a58b5f3f7035f9e0abb1172
Ludwin Garcia <lgarcia@cognits.co>
2015-05-22 15:31:44 -0600

    Plugin version 2, se agrego ejecucion por comandos y se elimino el index.html

--- js/background.js
+++ js/background.js
@@ -1,37 +1,47 @@
-function clickHandler(e) {
-    chrome.extension.sendMessage({directive: "popup-click"}, function(response) {
-        this.close(); // close the popup when the background finishes processing request
-    });
-};
+var tabId = 0 /*Guardara el id del tab de udacity*/
+var ValueFirstTime = true /*Verifica si es primera vez que se ejecuta el script*/
 
-function Reload(){
+function Reload(FistTime){
     /* Este procedmiento llama y ejecuta el codigo de script.js */
-    chrome.tabs.getSelected(null, function(tab) {
-        chrome.tabs.executeScript(tab.id, {file: "js/script.js"});
-    });
+    if (FistTime === true) {
+        chrome.tabs.getSelected(null, function(tab) {
+            tabId = tab.id
+            ValueFirstTime = false
+            chrome.tabs.executeScript(tab.id, {file: "js/script.js"});
+        });
+    } else if (FistTime === false){ /*Si ya no es la primera vez 
+    mantiene ejecutando el script en el tab que se ejecuto por primera vez */
+        chrome.tabs.executeScript(tabId, {file: "js/script.js"});
+    };
 };
 
-/* Este evento permite tomar los elementos del DOM en Index.html*/
-document.addEventListener('DOMContentLoaded', function() {
+/*Funcion que permite parar el plugin al cerrar el tab*/
+function Stop(CloseTabID) {
+    if (CloseTabID === tabId) {
+        alert("Search Projects Stop")
+        tabId = 0
+        ValueFirstTime = true
+        clearInterval(SearchProjects);
+    }
+}
 
-    /* Guardamos el boton start */
-    var CheckStartButton = document.getElementById('start');
-    var CheckStopButton = document.getElementById('stop');
-    var TimeRefresh = document.getElementById('time-refresh');
 
-    /* creamos el evento si se clickea el boton start*/
-    CheckStartButton.addEventListener('click', function() {
-        if (TimeRefresh.value < 1) {
-            alert("Ingreso Erroneo");
-        } else {
-            setInterval(function(){Reload()}, TimeRefresh.value*1000);
-        };
-    }, false);
+/* Chrome commands permite tomar los comandos mandados desde el teclado
+a chrome */
+chrome.commands.onCommand.addListener(function(command) {
+    if(command == "Play"){
+        alert("Search Projects Start")
+        SearchProjects = setInterval(function(){Reload(ValueFirstTime)}, 3000);
+    }else if(command == "Stop"){
+        //stop function here
+        alert("Search Projects Stop")
+        tabId = 0
+        ValueFirstTime = true
+        clearInterval(SearchProjects);
+    }
+ });
 
-    /* creamos el evento si se clickea el boton false*/
-    CheckStopButton.addEventListener('click', function() {
-        clickHandler();
-    }, false);
-
-}, false);
-
+/* Metodo que controla las tab que se cierran*/
+chrome.tabs.onRemoved.addListener(function(CloseTabID) {
+    Stop(CloseTabID);
+});
